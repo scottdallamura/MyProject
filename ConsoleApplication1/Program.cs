98f5340765456cefbad1230f4b9fef6d51f06f78
scott dallamura <scdallam@microsoft.com>
2014-12-11 08:38:48 -0500

    updated message

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("hello");
+            Console.WriteLine("hello!");
         }
     }
 }
