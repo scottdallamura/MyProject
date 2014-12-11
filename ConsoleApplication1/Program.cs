f005d76febef0b4dbcaf518ed21e91674b895bad
scott dallamura <scdallam@microsoft.com>
2014-12-11 08:22:41 -0500

    better, but not much

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1 {
     class Program {
         static void Main(string[] args) {
-            oops
+            oops;
         }
     }
 }
